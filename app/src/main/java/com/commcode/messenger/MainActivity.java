7ae67751449ca368581f12527ac60e77e29690bb
DennsHartstock <dennis.hartstock@web.de>
2022-12-12 16:32:06 +0100

    using signIn and resetPassword

--- app/src/main/java/com/commcode/messenger/MainActivity.java
+++ app/src/main/java/com/commcode/messenger/MainActivity.java
@@ -3,10 +3,12 @@ package com.commcode.messenger;
 import android.os.Bundle;
 import android.util.Log;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,35 +22,36 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         firebaseAuth = FirebaseAuth.getInstance();
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-        FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
-        if (firebaseUser != null) {
-            Log.d(LOG_TAG, "Authorized " + firebaseUser.getUid());
-        } else {
-            Log.d(LOG_TAG, "Not authorized");
-        }
-        firebaseAuth.signOut();
-//        firebaseAuth.createUserWithEmailAndPassword("email@web.de", "111111")
-//                .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
-//            @Override
-//            public void onSuccess(AuthResult authResult) {
-//                FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
-//                if (firebaseUser != null) {
-//                    Log.d(LOG_TAG, "Authorized");
-//                } else {
-//                    Log.d(LOG_TAG, "Not authorized");
-//                }
-//            }
-//        }).addOnFailureListener(new OnFailureListener() {
-//            @Override
-//            public void onFailure(@NonNull Exception e) {
-//                Log.d(LOG_TAG, e.getMessage());
-//                Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+//        firebaseAuth.signInWithEmailAndPassword("dennis.hartstock@web.de", "111111")
+//                .addOnFailureListener(e -> {
+//                    Log.d(LOG_TAG, e.getMessage());
+//                    Toast.makeText(MainActivity.this,
+//                            e.getMessage(),
+//                            Toast.LENGTH_SHORT).show();
+//                });
+//
+//        firebaseAuth.addAuthStateListener(firebaseAuth -> {
+//            FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
+//            if (firebaseUser != null) {
+//                Log.d(LOG_TAG, "Authorized " + firebaseUser.getUid());
+//            } else {
+//                Log.d(LOG_TAG, "Not authorized");
 //            }
 //        });
+//        firebaseAuth.signOut();
+
+        firebaseAuth.sendPasswordResetEmail("dennis.hartstock@web.de")
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void unused) {
+                        Log.d(LOG_TAG, "Email send");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.d(LOG_TAG, e.getMessage());
+                    }
+                });
     }
 }
