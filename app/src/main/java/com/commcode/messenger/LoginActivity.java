d07f93769a414f8dab8fabffb35c3551e458efe1
DennsHartstock <dennis.hartstock@web.de>
2022-12-13 06:31:42 +0100

    start LoginActivity logic adding

--- app/src/main/java/com/commcode/messenger/LoginActivity.java
+++ app/src/main/java/com/commcode/messenger/LoginActivity.java
@@ -1,16 +1,46 @@
 package com.commcode.messenger;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private EditText etEmail;
+    private EditText etPassword;
+    private Button btLogIn;
+    private TextView tvForgetPassword;
+    private TextView tvSignIn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        initViews();
 
+        btLogIn.setOnClickListener(view -> {
+            String email = etEmail.getText().toString().trim();
+            String password = etPassword.getText().toString().trim();
+            //login
+        });
+
+        tvForgetPassword.setOnClickListener(view -> {
+            //launch intent to forget password screen
+        });
+
+        tvSignIn.setOnClickListener(view -> {
+            //launch intent to register screen
+        });
+    }
+
+    private void initViews() {
+        etEmail = findViewById(R.id.etEmail);
+        etPassword = findViewById(R.id.etPassword);
+        btLogIn = findViewById(R.id.btLogIn);
+        tvForgetPassword = findViewById(R.id.tvForgetPassword);
+        tvSignIn = findViewById(R.id.tvSignIn);
     }
 }
