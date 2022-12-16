2707319d9ef59cfa8aa248b0581d7a12ee277ef1
DennsHartstock <dennis.hartstock@web.de>
2022-12-16 17:09:36 +0100

    add User

---
+++ app/src/main/java/com/commcode/messenger/User.java
@@ -0,0 +1,26 @@
+package com.commcode.messenger;
+
+public class User {
+
+    private String id;
+    private String name;
+    private Boolean isOnline;
+
+    public User(String id, String name, Boolean isOnline) {
+        this.id = id;
+        this.name = name;
+        this.isOnline = isOnline;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Boolean getOnline() {
+        return isOnline;
+    }
+}
