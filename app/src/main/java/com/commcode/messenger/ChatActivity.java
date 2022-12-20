32e48faebbfd5b08824dc276d019bfe85a6cb057
DennsHartstock <dennis.hartstock@web.de>
2022-12-20 16:57:14 +0100

    implement message sending

--- app/src/main/java/com/commcode/messenger/ChatActivity.java
+++ app/src/main/java/com/commcode/messenger/ChatActivity.java
@@ -7,13 +7,11 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.RecyclerView;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class ChatActivity extends AppCompatActivity {
 
@@ -32,6 +30,47 @@ public class ChatActivity extends AppCompatActivity {
     private String otherUserId;
 
     private ChatViewModel viewModel;
+    private ChatViewModelFactory viewModelFactory;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+        initViews();
+
+        currentUserId = getIntent().getStringExtra(EXTRA_CURRENT_USER_ID);
+        otherUserId = getIntent().getStringExtra(EXTRA_OTHER_USER_ID);
+        viewModelFactory = new ChatViewModelFactory(currentUserId, otherUserId);
+        viewModel = new ViewModelProvider(this, viewModelFactory).get(ChatViewModel.class);
+        messagesAdapter = new MessagesAdapter(currentUserId);
+        rvMessages.setAdapter(messagesAdapter);
+
+        observeViewModel();
+        ivSendMessage.setOnClickListener(v -> {
+            Message message = new Message(
+                    etMessage.getText().toString().trim(),
+                    currentUserId,
+                    otherUserId
+            );
+            viewModel.sendMessage(message);
+        });
+    }
+
+    private void observeViewModel() {
+        viewModel.getMessages().observe(this, messages -> messagesAdapter.setMessages(messages));
+        viewModel.getError().observe(this, errorMessage -> {
+            if (errorMessage != null) {
+                Toast.makeText(ChatActivity.this, errorMessage, Toast.LENGTH_SHORT).show();
+            }
+        });
+        viewModel.getIsMessageSent().observe(this, isSent -> {
+            if (isSent) {
+                etMessage.setText("");
+            }
+        });
+        viewModel.getOtherUser().observe(this, user -> tvTitle.setText(user.getName()));
+    }
 
     public static Intent newIntent(Context context, String currentUserId, String otherUserId) {
         Intent intent = new Intent(context, ChatActivity.class);
@@ -46,38 +85,5 @@ public class ChatActivity extends AppCompatActivity {
         rvMessages = findViewById(R.id.rvMessages);
         etMessage = findViewById(R.id.etMessage);
         ivSendMessage = findViewById(R.id.ivSendMessage);
-    }
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_chat);
-        initViews();
-
-        viewModel = new ViewModelProvider(this).get(ChatViewModel.class);
-        currentUserId = getIntent().getStringExtra(EXTRA_CURRENT_USER_ID);
-        otherUserId = getIntent().getStringExtra(EXTRA_OTHER_USER_ID);
-        messagesAdapter = new MessagesAdapter(currentUserId);
-        rvMessages.setAdapter(messagesAdapter);
-
-        List<Message> messages = new ArrayList<>();
-        for (int i = 0; i < 10; i++) {
-            Message myMessage = new Message(
-                    "My message " + i,
-                    currentUserId,
-                    otherUserId
-            );
-            messages.add(myMessage);
-        }
-
-        for (int i = 0; i < 10; i++) {
-            Message otherMessage = new Message(
-                    "Other message " + i,
-                    otherUserId,
-                    currentUserId
-            );
-            messages.add(otherMessage);
-        }
-        messagesAdapter.setMessages(messages);
     }
 }
